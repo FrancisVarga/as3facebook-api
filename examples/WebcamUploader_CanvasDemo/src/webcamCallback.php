6f8c391c8d2206f61a71097d798793aa0f5ede4e
FrancisVarga <github@varga-net.com>
2010-02-24 18:01:55 +0100

    import to github

---
+++ examples/WebcamUploader_CanvasDemo/src/webcamCallback.php
@@ -0,0 +1,13 @@
+<fb:add-section-button section="profile"/>
+
+<hr />
+<!-- 
+Set the swfsrc and imgsrc to an URL on your server.
+-->
+
+<fb:swf id='facebookApp'
+		swfsrc='webcamUploader.swf'
+		imgsrc='webcamUploader.jpg'
+		swfbgcolor='006599'
+		width='214' 
+		height='255' />
